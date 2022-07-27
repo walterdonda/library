7e2ea6f52344bab90942032e9174b639a5352d02
Walter Donda <walteroscardonda@gmail.com>
2022-07-27 18:54:02 -0300

    Cambios en el manifes, permisos de seguridad

--- library_app/__manifest__.py
+++ library_app/__manifest__.py
@@ -10,14 +10,14 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Walter Donda",
+    'website': "https://github.com/walterdonda/library",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Services/Library',
+    'version': '15.0.1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -25,8 +25,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/library_security_groups.xml',
+        'views/library_menu.xml',
     ],
     # only loaded in demonstration mode
     'demo': [

a9d5b23c78e468a112a425d63bb9b64f1a237b5d
Walter Donda <walteroscardonda@gmail.com>
2022-07-27 23:01:57 -0300

    modified:   library_app/models/__init__.py 	modified:   library_app/models/library_book.py

---
+++ library_app/models/library_book.py
@@ -0,0 +1,30 @@
+from odoo import fields, models
+
+class LibraryBook(models.Model):
+    _name = 'library.book'
+    _description = 'Libro'
+    
+    name = fields.Char(string='Título', required=True)
+    
+    isbn = fields.Char(
+        string='ISBN',
+        required=True)
+    
+    date_release = fields.Date(
+        string='Fecha de publicación',
+    )
+
+    
+    active = fields.Boolean(
+        string='Activo?',
+        default = True,
+        help='Indica si el libro se encuentra en estado activo.'
+        
+    )
+    
+
+    
+    
+    
+    
+  

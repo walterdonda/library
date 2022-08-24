73f640cf0cca5c2ac8e0b993645321b86374bf86
Walter Donda <walteroscardonda@gmail.com>
2022-08-23 22:41:58 -0300

    modified:   library_member/models/library_book.py

--- library_member/models/library_book.py
+++ library_member/models/library_book.py
@@ -1,8 +1,11 @@
+from operator import index
 from odoo import fields, models
 
 class Book(models.Model):
 
     _inherit = 'library.book'
 
-    is_available = fields.Boolean("Está disponible para prestar?") 
+    is_available = fields.Boolean("Está disponible para prestar?")
+    isbn = fields.Char(help='Usar un ISBN-13 o un ISBN-10 válido')
+    publisher_id = fields.Many2one(index=True) 
     

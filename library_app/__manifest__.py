94ade94ccd8f06f86fc2df2ed9a587c305468cee
Walter Donda <walteroscardonda@gmail.com>
2022-08-26 20:04:17 -0300

    Creo un nuevo filtro con xpath

--- library_app/__manifest__.py
+++ library_app/__manifest__.py
@@ -12,7 +12,6 @@
     'author': "Walter Donda",
     'website': "https://github.com/walterdonda/library",
 
-    # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Services/Library',

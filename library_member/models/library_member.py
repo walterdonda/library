8f5d4d65df0f102bb61b1efb5a4a7ef125ab5cfa
Walter Donda <walteroscardonda@gmail.com>
2022-09-02 18:37:34 -0300

    modified:   library_member/models/library_member.py 	modified:   library_member/views/book_view.xml

--- library_member/models/library_member.py
+++ library_member/models/library_member.py
@@ -8,13 +8,17 @@ class Member(models.Model):
     _name = "library.member"
 
     _description = "Library Member"
-    
+
     card_number = fields.Char(string="Número de socio")
 
     partner_id = fields.Many2one(
         "res.partner", delegate=True, ondelete="cascade", required=True
     )
-    
+
     _sql_constraints = [
-        ("card_number_unique", "UNIQUE(card_number)", "El número de socio debe ser único"),
+        (
+            "card_number_unique",
+            "UNIQUE(card_number)",
+            "El número de socio debe ser único",
+        ),
     ]
